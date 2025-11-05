b5b07fb39bded026fbde4b112062450dafd9e69c
Zhou Mi <mizhouis@gmail.com>
2025-11-05 16:47:16 +0100

    fix(megaphone.cpp): fix segfault bug

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,23 +2,26 @@
 #include <string>
 #include <cctype>
 
-int	main(void)
+void	print_toupperstr(std::string str)
 {
-	std::string				line;
 	std::string::iterator	it;
 
-	std::getline(std::cin, line);
-	if (line.empty() == true)
+	for (it = str.begin(); it != str.end(); it++)
+	{
+		*it = std::toupper(*it);
+		std::cout << *it;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	else
 	{
-		for (it = line.begin(); it != line.end(); it++)
-		{
-			if (std::islower(*it) == true)
-				*it = std::toupper(*it);
-			std::cout << *it;
-		}
+		while (argv++ && *argv)
+			print_toupperstr(*argv);
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;
 	return (EXIT_SUCCESS);
 }
